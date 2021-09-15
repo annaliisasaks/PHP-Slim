1bdf7a3c6131b18e6d9aec30e7c145305ef57461
annaliisasaks <anna-liisa.saks@khk.ee>
2021-09-15 10:56:31 +0300

    Added error handling

---
+++ src/Controller/ExceptionController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Psr7\Response;
+
+class ExceptionController extends Controller {
+	public function notFound(Request $request) {
+		$response = new Response;
+		return $this->render($response, 'notfound.html');
+	}
+}
